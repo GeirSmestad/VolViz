dadd778354915b4944d59c7530bc1a0bbc1b6d23
Geir Smestad <geir.smestad@gmail.com>
2018-10-09 21:09:44 +0200

    Added shell of volume data structure

---
+++ Data/Volume.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolViz.Data
+{
+    public class Volume
+    {
+        public int XSize;
+        public int YSize;
+        public int ZSize;
+
+        public int[,,] Contents;
+
+        /// <summary>
+        /// Initialize an empty volume
+        /// </summary>
+        public Volume(int xSize, int ySize, int zSize)
+        {
+            this.XSize = xSize;
+            this.YSize = ySize;
+            this.ZSize = zSize;
+
+            this.Contents = new int[xSize, ySize, zSize];
+        }
+
+        public static Volume LoadFromDatFile(string filename)
+        {
+            return new Volume(1, 1, 1);
+        }
+    }
+}
